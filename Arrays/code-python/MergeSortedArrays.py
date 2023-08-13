614f5fd733aa4062321fbac2493711ff346ea5be
Jared Thompkins <jaredthompkins21@gmail.com>
2023-08-13 13:10:34 -0500

    add python array solutions (#4)
    * add python array solutions
    
    * add MaxSubArray & MergeSortedArrays ref
    
    ---------
    
    Co-authored-by: Vasu Jhawar <59659588+vasujhawar2001@users.noreply.github.com>

---
+++ Arrays/code-python/MergeSortedArrays.py
@@ -0,0 +1,19 @@
+# Check out this problem on leetcode at --> https://leetcode.com/problems/merge-sorted-array/
+
+class MergeSortedArrays:
+    def merge(self, nums1, m, nums2, n):
+        p1, p2, p = m - 1, n - 1, m + n - 1
+
+        while p1 >= 0 and p2 >= 0:
+            if nums1[p1] > nums2[p2]:
+                nums1[p] = nums1[p1]
+                p1 -= 1
+            else:
+                nums1[p] = nums2[p2]
+                p2 -= 1
+            p -= 1
+
+        while p2 >= 0:
+            nums1[p] = nums2[p2]
+            p2 -= 1
+            p -= 1
--- Contributors.md
+++ Contributors.md
@@ -1,4 +1,5 @@
 # Contributors
 - [![Vasu Jhawar](https://avatars.githubusercontent.com/u/59659588?v=4)](https://github.com/vasujhawar2001)
 - [Sayak Nandy](https://github.com/Sayak123456)
+- [Jared Thompkins](https://github.com/Jared-Thompkins)
 - [Syed Abdul Malik](https://github.com/Syed-Abdul-Malik)
